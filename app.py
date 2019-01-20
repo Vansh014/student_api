e9bca0192bfa497428208fe8792a533474161ccc
Vansh014 <45897295+Vansh014@users.noreply.github.com>
2019-01-20 21:09:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Welcome to student records api"
+	return "BABY I LOVE YOU :*"
 
 @app.route('/students/', methods=['GET','POST'])
 def student_list():
